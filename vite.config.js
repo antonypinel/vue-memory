866ee3606a4e5bdf562de6fd2532be0e33fb41b2
0ri0n09 <jonathan.jaremczuk@ynov.com>
2023-04-17 16:09:18 +0200

    sw

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-memory/',
-  plugins: [
-      vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        name: 'MemoryPWA',
-        short_name: 'PWA',
-        start_url: '.',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#4DBA87'
-      }
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: '/vue-memory/',
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
+            manifest: {
+                name: 'MemoryPWA',
+                short_name: 'PWA',
+                start_url: '.',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#4DBA87'
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
