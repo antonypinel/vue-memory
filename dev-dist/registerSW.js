30ce8079bea6766b65171e121e29be113e23856c
antony <antonypinel@gmail.com>
2023-04-24 11:54:40 +0200

    Fix service-worker /!\ DON'T FORGET TO BUILD

--- dev-dist/registerSW.js
+++ dev-dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/vue-memory/dev-sw.js?dev-sw', { scope: '/vue-memory/', type: 'classic' })
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
